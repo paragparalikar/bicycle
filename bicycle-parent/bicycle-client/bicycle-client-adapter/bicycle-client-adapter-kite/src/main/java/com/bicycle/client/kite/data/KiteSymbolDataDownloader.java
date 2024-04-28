80da7ed7a980a877d83e3bd7802606f4a46505b8
Parag Paralikar <parag.paralikar@gmail.com>
2024-04-28 19:25:51 +0530

    RemoveTransferFiles

--- bicycle-parent/bicycle-client/bicycle-client-adapter/bicycle-client-adapter-kite/src/main/java/com/bicycle/client/kite/data/KiteSymbolDataDownloader.java
+++ bicycle-parent/bicycle-client/bicycle-client-adapter/bicycle-client-adapter-kite/src/main/java/com/bicycle/client/kite/data/KiteSymbolDataDownloader.java
@@ -1,21 +1,25 @@
 package com.bicycle.client.kite.data;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import com.bicycle.client.kite.adapter.KiteBrokerClientFactory;
 import com.bicycle.client.kite.adapter.KiteSymbolDataProvider;
-import com.bicycle.core.bar.Bar;
 import com.bicycle.core.bar.Timeframe;
 import com.bicycle.core.bar.provider.BarDataProvider;
 import com.bicycle.core.bar.provider.LoadBalancedBarDataProvider;
 import com.bicycle.core.bar.provider.query.BarQuery;
+import com.bicycle.core.bar.provider.query.BarQueryTransformer;
 import com.bicycle.core.bar.repository.BarRepository;
 import com.bicycle.core.bar.repository.FileSystemBarRepository;
 import com.bicycle.core.portfolio.FileSystemPortfolioRepository;
@@ -34,6 +38,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class KiteSymbolDataDownloader implements Runnable {
 	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+	
+	public static void main2(String[] args) throws IOException {
+		final List<Path> m10Files = Files.list(Paths.get("C:\\Users\\parag\\.bicycle\\bars\\NSE\\M10"))
+				.map(Path::getFileName)
+				.collect(Collectors.toList());
+		final List<Path> dailyFiles = Files.list(Paths.get("C:\\Users\\parag\\.bicycle\\bars\\NSE\\D"))
+				.map(Path::getFileName)
+				.toList();
+		m10Files.removeAll(dailyFiles);
+		m10Files.forEach(System.out::println);
+	}
 	
 	public static void main1(String[] args) throws Exception {
 		final SymbolDataProvider symbolDataProvider = new KiteSymbolDataProvider().equitiesOnly();
@@ -55,41 +70,49 @@ public class KiteSymbolDataDownloader implements Runnable {
 		final SymbolDataProvider symbolDataProvider = new KiteSymbolDataProvider().equitiesOnly();
 		final SymbolRepository symbolRepository = new CacheSymbolRepository(symbolDataProvider);
 		final BarRepository barRepository = new FileSystemBarRepository(symbolRepository);
+		final BarQueryTransformer transformer = new BarQueryTransformer();
 		
-		for(Timeframe timeframe : Arrays.asList(Timeframe.values()).reversed()) {
-			final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
-			for(Symbol symbol : symbolRepository.findByExchange(Exchange.NSE)) {
-				executorService.execute(KiteSymbolDataDownloader.builder()
-						.symbol(symbol)
-						.timeframe(timeframe)
-						.barRepository(barRepository)
-						.barDataProvider(barDataProvider)
-						.build());
-			}
-			executorService.shutdown();
-			executorService.awaitTermination(10, TimeUnit.HOURS);
+		final ExecutorService executorService = Executors.newFixedThreadPool(20);
+		for(Symbol symbol : symbolRepository.findByExchange(Exchange.NSE)) {
+			executorService.execute(KiteSymbolDataDownloader.builder()
+					.symbol(symbol)
+					.timeframe(Timeframe.M5)
+					.transformer(transformer)
+					.barRepository(barRepository)
+					.barDataProvider(barDataProvider)
+					.build());
 		}
+		executorService.shutdown();
+		executorService.awaitTermination(10, TimeUnit.HOURS);
 	}
 	
 	private final Symbol symbol;
 	private final Timeframe timeframe;
 	private final BarRepository barRepository;
 	private final BarDataProvider barDataProvider;
+	private final BarQueryTransformer transformer;
 
 	@Override
 	public void run() {
 		final ZonedDateTime lastDownloadDate = Dates.toZonedDateTime(barRepository.getLastDownloadDate(symbol, timeframe));
 		final ZonedDateTime fromDate = lastDownloadDate.plusMinutes(timeframe.getMinuteMultiple());
 		final ZonedDateTime toDate = ZonedDateTime.now();
+		
 		final BarQuery barQuery = BarQuery.builder()
 				.symbol(symbol)
 				.timeframe(timeframe)
 				.from(fromDate)
 				.to(toDate)
 				.build();
-		final List<Bar> bars = barDataProvider.get(barQuery);
-		barRepository.persist(symbol, timeframe, bars);
-		System.out.printf("%s %-5s %-15s %d\n", FORMATTER.format(fromDate), timeframe, symbol.code(), bars.size());
+		
+		transformer.transform(barQuery)
+			.map(barDataProvider::get)
+			.ifPresentOrElse(bars -> {
+				barRepository.persist(symbol, timeframe, bars);
+				System.out.printf("%s %-5s %-15s %d\n", FORMATTER.format(fromDate), timeframe.name(), symbol.code(), bars.size());
+			}, () -> {
+				System.out.printf("Nothing to download - %s - %s\n", symbol.code(), timeframe.name());
+			});
 	}
 
 }
